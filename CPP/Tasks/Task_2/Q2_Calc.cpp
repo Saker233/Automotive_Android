63a5edc3cf038c70f30b587a67cd2511fcfcd9f9
saker <mohamedsaker233@gmail.com>
2024-07-10 14:20:59 +0300

    CPP task2

--- CPP/Tasks/Task_2/Q2_Calc.cpp
+++ CPP/Tasks/Task_2/Q2_Calc.cpp
@@ -2,17 +2,13 @@
 #include <cmath>
 #include <cctype>
 
-
-
-
 class Calc
 {
     int first;
     int second;
     char op;
 
-    public:
-
+public:
     void set_first(int x)
     {
         first = x;
@@ -28,111 +24,86 @@ class Calc
 
     int ADD()
     {
-        int res = 0;
-
-        res = first + second;
-
-        return res;
+        return first + second;
     }
 
     int SUB()
     {
-        int res = 0;
-
-        res = first - second;
-
-        return res;
+        return first - second;
     }
 
     int MULT()
     {
-        int res = 0;
-
-        res = first * second;
-
-        return res;
+        return first * second;
     }
 
     double DIV()
     {
-        int res = 0;
-
-        res = first / second;
-
-        return res;
+        return static_cast<double>(first) / second;
     }
 
     int POW()
     {
-        int res = 0; 
-        res = first ^ second;
-
-        return res;
+        return pow(first, second);
     }
 
     double SQRT()
     {
-        int res = 0;
-
-        res = sqrt(first);
-
-        return res;
+        return sqrt(first);
     }
-
-
-
 };
-
 
 int main()
 {
     Calc c;
 
-
-    int first_num=0;
-    int second_num= 0;
+    int first_num = 0;
+    int second_num = 0;
     char operation = 0;
-    std::cout<< "Please Enter The First Number";
-    std::cin>>first_num;
+    std::cout << "Please Enter The First Number : ";
+    std::cin >> first_num;
 
-    std::cout<< "Please Enter The operation";
-    std::cin>>operation;
+    std::cout << "Please Enter The operation : ";
+    std::cin >> operation;
 
-    std::cout<< "Please Enter The Second Number";
-    std::cin>>second_num;
+    if (operation != 's')
+    {
+        std::cout << "Please Enter The Second Number : ";
+        std::cin >> second_num;
+        c.set_second(second_num);
+    }
 
     c.set_first(first_num);
 
-    c.set_second(second_num);
+    c.set_op(operation);
 
-    c.set_op(operation);\
-
-    int result = 0;
+    double result = 0;
 
     switch (operation)
     {
     case '+':
-        result = c.ADD(first_num, second_num);
+        result = c.ADD();
         break;
     case '-':
-        result = c.SUB(first_num, second_num);
+        result = c.SUB();
         break;
     case '*':
-        result = c.MULT(first_num, second_num);
+        result = c.MULT();
         break;
     case '/':
-        result = c.DIV(first_num, second_num);
+        result = c.DIV();
         break;
     case 'p':
-        result = c.POW(first_num, second_num);
+        result = c.POW();
         break;
-    
+
     case 's':
-        result = c.SQRT(first_num);
+        result = c.SQRT();
         break;
     default:
+        std::cout << "Invalid Operation" << std::endl;
         break;
     }
 
-    std::cout <<result << std::endl;
+    std::cout << result << std::endl;
 }
